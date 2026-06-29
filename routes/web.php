7cd64b1936091e2e6da656b7331078f8b4ad9eb2
ArmanAp244 <armanap244@gmail.com>
2026-06-29 18:46:46 +0330

    Add New controller And use an un used .blade

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BasicController;
 Route::get('/',[HomeController::class,'index']);
 Route::get('/salam',[HomeController::class,'salam']);
+route::get('/speed',[BasicController::class,'digi']);
