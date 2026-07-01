ba29b71e79431969141fa0e4865608dbdda89c49
ArmanAp244 <armanap244@gmail.com>
2026-07-01 19:57:29 +0330

    Added some sort of coding. Dont know why its working

--- resources/views/pro13.blade.php
+++ resources/views/pro13.blade.php
@@ -4,8 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>proMax</title>
+
+
+
+   <button id="btn"></button>
 </head>
 <body>
-    <h1 style = color:brown margin> ali ali </h1>
+<script>function me () {
+    alert(porteghal)
+    let x = document.getElementById("btn").value 
+    
+class Fruit {
+    constructor(fasl, taste, color, price) {
+        this.fasl = fasl;
+        this.taste = taste;
+        this.color = color;
+        this.price = price;
+    }
+}
+
+function s() {
+    let x = document.getElementById("btn").value;
+
+    const porteghal = new Fruit(
+        "paiz",
+        "shirin",
+        "orange",
+        "1000tkiluyi"
+    );
+
+    alert(porteghal.color);
+}
+
+
+
+}
+
+
+
+
+document.getElementById("btn").innerHTML = fruit</script>
 </body>
 </html>
